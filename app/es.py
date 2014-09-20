05f1ba60ebbf2c035d52d668d8d06fb714ae0e32
Alan Chen <alanchen@nyu.edu>
2014-09-20 18:36:20 -0400

    Indexed Precincts via Elasticsearch

--- app/es.py
+++ app/es.py
@@ -1,32 +1,29 @@
+from elasticsearch import Elasticsearch
 import MySQLdb
-import csv
-from pyelasticsearch import ElasticSearch
+import logging
 
-es = ElasticSearch('http://localhost:9200/')
-
-
-def importdata():
+logging.basicConfig(level=logging.INFO)
+def index_crime():
+    es = Elasticsearch('http://localhost:9200/')
     db = MySQLdb.connect('localhost', 'root', 'password', 'scout')
     cur = db.cursor()
-    with open('data/precinct_data.csv', 'rb') as fh:
-        reader = csv.DictReader(fh)
-        for row in reader:
-            print row
-            precinct = row['precinct'].strip().split()[0][:-2]
-            print precinct
-            s = "INSERT INTO precinct (name, phone, address, latitude, longitude, borough) VALUES ('%s', '%s', '%s', %s, %s, '%s');" %(precinct, row['phone'].strip(), row['address'].strip(), row['lat'].strip(), row['lng'].strip(), row['borough'].strip())
-            cur.execute(s)
-            db.commit()
-
-    with open('data/monthly.csv', 'rb') as mh:
-        reader = csv.DictReader(mh)
-        for row in reader:
-            precinct = row['Precinct'].strip().split()[0][:-2]
-            s = "INSERT INTO PrecinctData (name, crime, total) VALUES ('%s', '%s', %s);" %(precinct, row['Crime'].strip(), row['Total'].strip())
-            cur.execute(s)
-            db.commit()
-    cur.close()
-    db.close()
+    cur.execute("SELECT * FROM precinct")
+    data = cur.fetchall()
+    for precinct in data:
+        es.index(
+                "locations",
+                "point",
+                {
+                    "pin": {
+                        "location": {
+                            "lat": precinct[3],
+                            "lng": precinct[4]
+                        },
+                        "tag": "crime",
+                        "info": {"pid": precinct[0], "address": precinct[2], "borough": precinct[5]}
+                    }
+                }
+        )
 
 if __name__ == "__main__":
-    importdata()
+    index_crime()
---
+++ app/insert.py
@@ -0,0 +1,28 @@
+import MySQLdb
+import csv
+
+def importdata():
+    db = MySQLdb.connect('localhost', 'root', 'password', 'scout')
+    cur = db.cursor()
+    with open('data/precinct_data.csv', 'rb') as fh:
+        reader = csv.DictReader(fh)
+        for row in reader:
+            print row
+            precinct = row['precinct'].strip().split()[0][:-2]
+            print precinct
+            s = "INSERT INTO precinct (name, phone, address, latitude, longitude, borough) VALUES ('%s', '%s', '%s', %s, %s, '%s');" %(precinct, row['phone'].strip(), row['address'].strip(), row['lat'].strip(), row['lng'].strip(), row['borough'].strip())
+            cur.execute(s)
+            db.commit()
+
+    with open('data/monthly.csv', 'rb') as mh:
+        reader = csv.DictReader(mh)
+        for row in reader:
+            precinct = row['Precinct'].strip().split()[0][:-2]
+            s = "INSERT INTO PrecinctData (name, crime, total) VALUES ('%s', '%s', %s);" %(precinct, row['Crime'].strip(), row['Total'].strip())
+            cur.execute(s)
+            db.commit()
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    importdata()
